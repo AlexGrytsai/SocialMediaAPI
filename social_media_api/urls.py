2d34758ba1b23dc2d2752610ac822d13926fab8f
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-26 12:11:17 +0300

    added endpoints for work with token to urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -16,9 +16,29 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        "api/v1/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
+    path(
+        "api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"
+    ),
+    path("api/v1/token/logout/", LogoutView.as_view(), name="token_logout")
 ] + debug_toolbar_urls()
