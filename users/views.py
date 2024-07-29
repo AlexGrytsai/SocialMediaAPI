58c1d318a64102b7aedc9af63e5bcb26248ce1db
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-29 12:17:23 +0300

    defined permissions for UserViewSet and made redirect when action == retrieve and pk == request.user.id -> me/

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets, generics
-from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponseRedirect
+from rest_framework import viewsets, generics, status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.reverse import reverse
 
 from users.models import User
-from users.serializers import UserSerializer, UserListSerializer, \
-    UserDetailSerializer
+from users.serializers import UserCreateSerializer, UserListSerializer, \
+    UserDetailSerializer, UserUpdateAddSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,22 +13,46 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated,]
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action == "list":
             return UserListSerializer
         if self.action == "retrieve":
             return UserDetailSerializer
-        return UserSerializer
+        return UserCreateSerializer
+
+    def get_permissions(self):
+        if self.request.method == ("PUT", "PATCH", "DELETE"):
+            return (IsAdminUser,)
+        return super().get_permissions()
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a user by their ID. If the user ID matches the current
+        user's ID, redirect to the 'users:me' endpoint.
+        Otherwise, call the parent class's retrieve method.
+        """
+        user_id = kwargs.get("pk")
+        if str(user_id) == str(self.request.user.id):
+            url = reverse("users:me", request=request)
+            return HttpResponseRedirect(
+                url,
+                status=status.HTTP_308_PERMANENT_REDIRECT
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated,]
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return UserDetailSerializer
+        return UserUpdateAddSerializer
