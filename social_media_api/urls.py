d771977636f16f19d8cd609d62e2e559d5db3e24
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-29 11:05:50 +0300

    added to the maine urls a path to media_url

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path
@@ -43,4 +44,6 @@ urlpatterns = [
     ),
     path("api/v1/token/logout/", LogoutView.as_view(), name="token_logout"),
     path("api/v1/", include("users.urls")),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
