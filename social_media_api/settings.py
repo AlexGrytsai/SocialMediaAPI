e0301b5fe49d2d43c64db28e6382e34b6f3c812d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-07-30 09:35:09 +0300

    added age verification at the serializer level (UserCreateSerializer and UserUpdateSerializer)

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7), #minutes=30
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
