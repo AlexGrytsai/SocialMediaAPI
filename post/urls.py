5750933cad4c0ccd60a3937be5c02ec62be3461b
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-01 17:03:30 +0300

    created PostViewSet and added a path to this view in urls

--- post/urls.py
+++ post/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from post.views import PostViewSet
+
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "post"
--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from post.models import Post
+from post.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
