cca5f3d4f1bf19a470e09cb13e8cced54cb335f7
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-02 11:34:52 +0300

    added get_serializer_class in PostViewSet and added select_related and prefetch_related

--- post/views.py
+++ post/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from post.models import Post
-from post.serializers import PostSerializer
+from post.serializers import PostSerializer, PostListSerializer, PostDetailSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().select_related("owner").prefetch_related(
+        "hashtags"
+    )
     serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        if self.action == "retrieve":
+            return PostDetailSerializer
+        return super().get_serializer_class()
