3f0e62a405c4259134e16e4f52ae954b6655b8c1
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-05 08:58:09 +0300

    created a new action - edit comment for PostViewSet

--- post/views.py
+++ post/views.py
@@ -26,7 +26,7 @@ class PostViewSet(viewsets.ModelViewSet):
             return PostListSerializer
         if self.action == "retrieve":
             return PostDetailSerializer
-        if self.action == "add_comment":
+        if self.action in ("add_comment", "edit_comment"):
             return CommentSerializer
         return super().get_serializer_class()
 
@@ -57,7 +57,7 @@ class PostViewSet(viewsets.ModelViewSet):
     @action(
         detail=True,
         methods=["POST"],
-        url_path="add-comment",
+        url_path="add-comment/",
         url_name="add-comment",
         permission_classes=[IsAuthenticated],
     )
@@ -71,3 +71,33 @@ class PostViewSet(viewsets.ModelViewSet):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=True,
+        methods=["GET", "PUT", "DELETE"],
+        url_path="edit-comment/(?P<comment_id>[^/.]+)",
+        url_name="edit-comment",
+        permission_classes=[IsAuthenticated],
+    )
+    def edit_comment(
+        self,
+        request: HttpRequest,
+        pk: int = None,
+        comment_id: int = None
+    ) -> HttpResponse:
+        current_user = self.request.user
+        comment = self.get_object().comments.get(id=comment_id)
+        if current_user != comment.owner:
+            return Response(
+                data={
+                    "message": "You don't have permission to edit this comment"
+                },
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer = self.get_serializer(
+            comment, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
